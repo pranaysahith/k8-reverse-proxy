0d81d148d279ffaafc50a3462b7537348c24c10a
NourEddineX <noureddinex@protonmail.com>
2020-09-02 17:21:09 +0200

    ssl modifications

--- basic-squid-config/run-openssl.sh
+++ basic-squid-config/run-openssl.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-openssl req -new -newkey rsa:2048 -sha256 -nodes -extensions v3_ca -out /tmp/fullcert.csr -keyout privkey.key -config openssl.cnf
-openssl x509 -req -days 3650 -in /tmp/fullcert.csr -signkey privkey.key \
- -out cert.crt -extfile openssl.cnf -extensions v3_req
-openssl x509 -trustout  -req -days 3650 -in /tmp/fullcert.csr -signkey privkey.key -out cert.crt
-cat privkey.key >> fullcert.pem
-cat cert.crt >> fullcert.pem
-
+openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -extensions v3_ca -keyout fullcert.pem -out fullcert.pem -config openssl.cnf
