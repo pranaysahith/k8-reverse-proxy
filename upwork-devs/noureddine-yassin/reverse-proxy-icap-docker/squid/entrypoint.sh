10799e9a51b8280c05c6370437548f3231c6de09
NourEddineX <noureddinex@protonmail.com>
2020-09-09 20:15:37 +0200

    dockerized squid reverse proxy + ICAP

---
+++ upwork-devs/noureddine-yassin/reverse-proxy-icap-docker/squid/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for domain in $( echo $ALLOWED_DOMAINS | tr ',' ' ' ) ; do
+    echo "acl allowed dstdomain $domain" >> /etc/squid/conf.d/allowed_backends.conf
+done
+
+sed -i "s,ICAP_URL,$ICAP_URL,g" /etc/squid/squid.conf
+sed -i "s,ROOT_DOMAIN,$ROOT_DOMAIN,g" /etc/squid/rewriter
+/usr/sbin/squid --foreground -f /etc/squid/squid.conf
