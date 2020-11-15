8b3c7f9cf9247a59a1e4a1afbaa6164fc05a6ff1
NourEddineX <noureddinex@protonmail.com>
2020-11-15 13:47:06 +0200

    Exclude mime types from ICAP filtering

--- stable-src/squid/entrypoint.sh
+++ stable-src/squid/entrypoint.sh
@@ -9,5 +9,11 @@ if [ -f /first-run ] ; then
 	sed -i "s,ICAP_URL,$ICAP_URL,g" /etc/squid/squid.conf
 	sed -i "s,ROOT_DOMAIN,$ROOT_DOMAIN,g" /etc/squid/rewriter
 	rm /first-run
+    if [ ! -z "$ICAP_EXCLUDE_MIME_TYPE" ] ; then
+        for mimetype in $( echo $ICAP_EXCLUDE_MIME_TYPE | tr ',' ' ' ) ; do
+            echo "acl noicap rep_mime_type $mimetype" >>  /etc/squid/conf.d/noicap.conf
+            sed -i '/noicap/ s/#//g' /etc/squid/squid.conf
+        done
+    fi
 fi
 /usr/sbin/squid --foreground -f /etc/squid/squid.conf
